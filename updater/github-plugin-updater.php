c3913479b3532d0d6de3ad82ce6783c02c2414d9
mahmut-seker-dev <mahmut.seker.1071@gmail.com>
2025-11-10 17:11:13 +0300

    v1.0.5

--- updater/github-plugin-updater.php
+++ updater/github-plugin-updater.php
@@ -5,7 +5,7 @@
  * WordPress eklentileri için GitHub üzerinden otomatik güncelleme kontrolü sağlar.
  * 
  * @package GSP_Connector
- * @version 1.0.45
+ * @version 1.0.5
  */
 
 if (!defined('ABSPATH')) {
